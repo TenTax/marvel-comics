e75e6a792a9f280402d6f5f519f224e2bcfa0122
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-12-03 20:08:14 +0300

    Preloader component created&changed

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,16 @@
+import { ROOT } from "../../constants/root";
+import classes from './Preloader.css';
+
+class Preloader {
+    render() {
+        const htmlWrapper = `
+            <div class="${classes.preloader__wrapper}">
+                <div class="${classes.preloader__item}"></div>
+            </div>
+        `;
+        
+        ROOT.innerHTML = htmlWrapper;
+    }
+}
+
+export default new Preloader();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import 'regenerator-runtime/runtime'
 import App from './components/App';
 import Comics from './components/Comics';
+import Preloader from './components/Preloader';
 
 (async () => {
+
+    Preloader.render();
+    
     await App.render();
 
     Comics.eventListener();
