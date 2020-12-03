ace25d12ade9aa221d65804192db0eaed947112c
TenTax <Kostya.dorokhov.2016@mail.ru>
2020-12-03 19:46:41 +0300

    Error component create&changed

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -3,41 +3,40 @@ import { getDataApi } from '../../utils/getDataApi';
 import { ROOT } from '../../constants/root';
 
 import classes from './Comics.css';
+import Error from '../Error/Error';
 
 class Comics {
-    async render() {
-        const data = await getDataApi.getData(URL_COMICS);
+    renderComics(data) {
+        let htmlContent = '';
 
-        if (data) {
+        data.forEach(({ id, title, thumbnail: { extension, path } }) => {
 
-            let htmlContent = '';
+            if (path.lastIndexOf(IMAGE_NOT_AVAILABLE) === -1) {
+                const imgSrc = path + '/' + STANDARD_XLARGE + '.' + extension;
+                const uri = URL_COMICS + '/' + id + '/characters';
 
-            data.forEach(({ id, title, thumbnail: { extension, path } }) => {
-
-                if (path.lastIndexOf(IMAGE_NOT_AVAILABLE) === -1) {
-                    const imgSrc = path + '/' + STANDARD_XLARGE + '.' + extension;
-                    const uri = URL_COMICS + '/' + id + '/characters';
-
-                    htmlContent += `
+                htmlContent += `
                         <li class="comics__item ${classes.comics__item}" data-uri="${uri}">
                             <span class="${classes.comics__name}">${title}</span>
                             <img class="${classes.comics__img}" src="${imgSrc}">
                         </li>
                     `;
-                }
-            });
+            }
+        });
 
-            const htmlWrapper = `
+        const htmlWrapper = `
                 <ul class="${classes.comics__container}">
                     ${htmlContent}
                 </ul>
             `;
 
-            ROOT.innerHTML = htmlWrapper;
+        ROOT.innerHTML = htmlWrapper;
+    }
 
-        } else {
-            console.log('bad');
-        }
+    async render() {
+        const data = await getDataApi.getData(URL_COMICS);
+
+        data ? this.renderComics(data) : Error.render();
     }
 
     eventListener() {
@@ -45,7 +44,7 @@ class Comics {
             item.addEventListener('click', e => {
                 const uri = item.getAttribute('data-uri');
 
-                console.log('sdf')
+                console.log(e.target)
             });
         });
     }
---
+++ src/components/Error/Error.js
@@ -0,0 +1,19 @@
+import { ROOT } from '../../constants/root';
+import classes from './Error.css';
+
+class Error {
+    render() {
+        const htmlWrapper = `
+            <div class="${classes.error__container}">
+                <span>
+                    <p class="${classes.error__alert}">Произошла ошибка!</p>
+                    <p class="${classes.error__alert}">Попробуйте зайти позже</p>
+                </span>
+            </div>
+        `;
+
+        ROOT.innerHTML = htmlWrapper;
+    }
+}
+
+export default new Error();
